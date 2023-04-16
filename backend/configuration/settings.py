c4b37d3280c194c2180b1a40ff696be7e4a17d7b
AntonSidorin <antonsidorin@list.ru>
2023-04-16 15:55:59 +0300

    Fix pagination

--- backend/configuration/settings.py
+++ backend/configuration/settings.py
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "NON_FIELD_ERRORS_KEY": "non_field_error",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 5000,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
     ],
